deefd280b2c1cc5ed7053a0dd27c5cc3cfe69b0d
Derrick Craven <derrick.craven@ldschurch.org>
2016-11-23 12:01:38 -0700

    Move api params into variables

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var request = require('request');
 
-var url = "https://www.rottentomatoes.com/api/private/v2.0/browse?page=1&limit=30&type=dvd-all&minTomato=70&sortBy=release&certified=true";
+var limit = 30;
+var minTomato = 70;
+var certified = true;
+
+var url = `https://www.rottentomatoes.com/api/private/v2.0/browse?page=1&limit=${limit}&type=dvd-all&minTomato=${minTomato}&sortBy=release&certified=${certified}`;
 
 request(url, function (error, response, body) {
     let movies = JSON.parse(body).results.filter((movie)=>{
